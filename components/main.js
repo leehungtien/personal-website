077d550de5bc17e5843e8668836a2c5872878887
Hung Tien <leehungtien@gmail.com>
2022-09-24 16:02:36 +0800

    page set up with navbar

---
+++ components/main.js
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (
+        <div className="w-full h-screen text-center">
+            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+                <div>
+                    <p className="uppercase text-sm tracking-widest text-gray">LET'S BUILD SOMETHING TOGETHER</p>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ components/navbar.js
@@ -0,0 +1,107 @@
+import React, {useState} from "react";
+import Link from "next/link";
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai';
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+
+export default function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <div className="fixed w-full h-20 shadow-xl z-[100]">
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+                <div>
+                    <ul className="hidden md:flex">
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase cursor-pointer hover:scale-105 ease-in duration-75">
+                                Home
+                            </li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase cursor-pointer hover:scale-105 ease-in duration-75">
+                                About
+                            </li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase cursor-pointer hover:scale-105 ease-in duration-75">
+                                Skills
+                            </li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase cursor-pointer hover:scale-105 ease-in duration-75">
+                                Projects
+                            </li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase cursor-pointer hover:scale-105 ease-in duration-75">
+                                Contact
+                            </li>
+                        </Link>
+                    </ul>
+                    <div onClick={handleNav} className="md:hidden cursor-pointer hover:scale-105 ease-in duration-75">
+                        <AiOutlineMenu size={25}/>
+                    </div>
+                </div>
+            </div>
+
+        {/* For small screens */}
+        <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ''}>
+            <div className={
+                    nav
+                     ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w=[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                     : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
+                <div>
+                    <div onClick={handleNav} className="float-right rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                        <AiOutlineClose/>
+                    </div>
+                </div>
+                <div className="border-b border-gray-300 my-2">
+                    <p className="w-[85%] md:w-[90%] py-4">Lets build something legendary together!</p>
+                </div>
+                <div className="py-2 flexflex-col">
+                    <ul className="uppercase">
+                        <Link href="/">
+                            <li className="py-4 text-sm">Home</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4 text-sm">About</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4 text-sm">Skills</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4 text-sm">Projects</li>
+                        </Link>
+                        <Link href="/">
+                            <li className="py-4 text-sm">Contact</li>
+                        </Link>
+                    </ul>
+                    <div className="pt-4">
+                        <p className="uppercase tracking-widest text-[#5651e5]">
+                            Let's Connect
+                        </p>
+                        <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-75">
+                                <FaLinkedinIn/>
+                            </div>
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-75">
+                                <FaGithub/>
+                            </div>
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-75">
+                                <AiOutlineMail/>
+                            </div>
+                            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-75">
+                                <BsFillPersonLinesFill/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    );
+}

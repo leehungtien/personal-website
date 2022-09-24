f713047a367c760fa5f1977c8e21ade85fdb9483
Hung Tien <leehungtien@gmail.com>
2022-09-24 19:36:28 +0800

    Added experience component

--- components/experience.js
+++ components/experience.js
@@ -1,3 +1,5 @@
+import userData from '../constants/data'
+
 export default function Experience() {
     return (
         <div className="w-full lg:h-screen p-2">
@@ -6,6 +8,45 @@ export default function Experience() {
                     Experience
                 </h1>
             </div>
+            <div className="mt-4">
+
+                <div className="grid grid-cols-1 max-w-xl mx-auto pt-20">
+                {/* Experience card */}
+                {userData.experience.map((exp, idx) => (
+                    <>
+                    <ExperienceCard
+                        title={exp.title}
+                        desc={exp.desc}
+                        year={exp.year}
+                        company={exp.company}
+                    />
+
+                    {/* Check if it's the last data */}
+                    {idx === userData.experience.length - 1 ? 
+                        <div className='mb-4'></div> : (
+                        <div className="divider-container flex flex-col items-center -mt-2">
+                        <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
+                            <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+                        </div>
+                        <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+                        </div>
+                    )}
+                    </>
+                ))}
+                </div>
+            </div>
         </div>
     );
 }
+const ExperienceCard = ({ title, desc, year, company }) => {
+    return (
+        <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-200 z-10 mx-4">
+            <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+                {year}
+            </h1>
+            <h1 className="font-semibold text-xl">{title}</h1>
+            <p>{company}</p>
+            <p className="text-gray-600 dark:text-gray-600 my-2">{desc}</p>
+        </div>
+    );
+  };
---
+++ constants/data.js
@@ -0,0 +1,23 @@
+const userData = {
+    experience: [
+        {
+            title: "Machine Learning Engineer Intern",
+            company: "DSO National Laboratories",
+            year: "2022",
+            desc: `• Scripted generation of synthetic images using Blender software’s Python API
+            • Utilized Data Augmentation techniques that improved test accuracy by at least 5%
+            • Trained Resnet50 model on generated synthetic data for Image Classification task`,
+        },
+        {
+            title: "Software Engineer Intern",
+            company: "Continental",
+            year: "2022",
+            desc: `• Spearheaded project that upgraded team’s Googletest framework based on Continental’s guidelines
+            • Developed Standard Operating Procedure (SOP) on upgrading process for team’s future reference
+            • Basic debugging of batch and shell script files
+            • Debugged automated test cases using Robot framework for Ethernet development`,
+        },
+    ]
+}
+
+export default userData;

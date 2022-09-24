68f5b2290430cb2e92e0cf9f085f94000c5fbaae
Hung Tien <leehungtien@gmail.com>
2022-09-24 22:25:49 +0800

    Fix for large top white area in large screen

--- components/experience.js
+++ components/experience.js
@@ -2,7 +2,7 @@ import userData from '../constants/data'
 
 export default function Experience() {
     return (
-        <div className="w-full lg:h-screen p-2">
+        <div id='experience' className="w-full p-2">
             <div className="max-w-[1240px] max-auto flex flex-col justify-center h-full">
                 <h1 className="text-5xl tracking-widest uppercase text-[#5651e5]">
                     Experience
